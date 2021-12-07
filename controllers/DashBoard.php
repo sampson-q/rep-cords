76ef9ae21c39420e3af31ef495e4c76719d93836
sampson-q <quarmysokpoli@gmail.com>
2021-12-07 20:36:05 +0000

    Called the AddClass function from here

--- controllers/DashBoard.php
+++ controllers/DashBoard.php
@@ -1,11 +1,21 @@
 <?php
-    session_start();
     include_once 'DatabaseConnection.php';
     include_once 'CrudOperation.php';
+    include_once '../models/AddClass.php';
 
     $classname = filter_input(INPUT_POST, 'classname');
-    if ($classname !== '' || $classname !== null) {
-        echo 'class_success';
+    if (!empty($classname)) {
+        $AddClass = new AddClass();
+        $AddClass -> setClassName($classname);
+
+        $crud = new CrudOperation();
+        if ($crud -> AddClass($AddClass)) {
+            echo 'class_added';
+        } else {
+            echo 'add_class_error';
+        }
+        
+        // echo 'class_added';
     } else {
         echo 'empty_class_name';
     }
