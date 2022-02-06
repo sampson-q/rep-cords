555ad7b493aaaf3ff68fd2131648336f5ed67b79
sampson-q <quarmysokpoli@gmail.com>
2022-02-06 01:58:21 +0000

    added index file to cover up pdf files from users

---
+++ cachedattends/index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Wrong Page</title>
+    </head>
+    <body>
+        <h1><marquee behavior="alternate">You have no business here!</marquee></h1>
+    </body>
+</html>
